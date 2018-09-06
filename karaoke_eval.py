7aee80f0ac2e85fee16d94e4d3525c4707ba68d3
LifeEshiniki <matchooooka@outlook.com>
2018-09-07 03:32:54 +0900

    クラスにしてまとめた

--- karaoke_eval.py
+++ karaoke_eval.py
@@ -1,4 +1,7 @@
 import librosa
+from librosa import display
+from librosa import core
+
 import madmom
 import numpy as np
 import scipy as sp
@@ -7,35 +10,49 @@ import os,sys
 import pandas as pd
 import seaborn as sns
 
-def reading_audio(file):
-    audio = input("オーディオファイルの名前を入力")
-    if not os.path.isfile("./" + audio):
-        print("ファイルがありません")
-    else:
-        try:
-            wav,fs = librosa.load("./" + audio,sr = 44100)
-            stftobj = librosa.stft(wav)
-            librosa.display.specshow(librosa.logamplitude(np.abs(stftobj) ** 2 , ref_power = np.max),y_axis = "log",x_axis = "time")
+
+class audio_obj:
+
+    def __init__(self):
+        self.audio = None   #ファイル
+        self.wav = None     #波形
+        self.is_read = False    #ファイルを読み込んだか
+
+
+    def read(self,file):
+        self.audio = file
+        if not os.path.isfile("./" + self.audio):
+            print("ファイルがありません")
+        else:
+            try:
+                wav, fs = librosa.load("./" + self.audio, sr=44100)
+                self.is_read = True
+            except OSError:
+                print("オーディオファイルではありません")
+
+
+    def show_wave_spectrogram(self):
+        if self.is_read == True:
+            stftobj = librosa.stft(self.wav)
+            librosa.display.specshow(np.log(np.abs(stftobj) ** 2), y_axis="log", x_axis="time")
             plt.title('Power spectrogram')
             plt.colorbar(format='%+2.0f dB')
             plt.tight_layout()
-        except OSError:
-            print("オーディオファイルではありません")
+            plt.show()
 
-
-class pitch_rating():
+class pitch_rating:
 
     def __init__(self):
         self.score = 0.0
 
 
 
-class rhythm_rating():
+class rhythm_rating
 
     def __init__(self):
         self.score = 0.0
 
-class volume_rating():
+class volume_rating:
 
     def __init__(self):
         self.score = 0.0
@@ -47,7 +64,9 @@ def overall_score_ranker(s1,s2,s3,w1,w2,w3):
 def main():
     print("hoge")
     file = input("オーディオファイルを入力してください")
-    reading_audio(file)
+    analyze = audio_obj()
+    analyze.read(file)
 
-if __name__ == "__main__ ":
+
+if __name__ == "__main__":
     main()
